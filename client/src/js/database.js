6f9920a7c924c70717614b4d49b9a73dd7a63483
Louis <louyoon89@gmail.com>
2023-02-17 10:58:34 -0800

    database putdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb =  async (content) => {
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction('jate', 'readwrite');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.put({ id: 1, value: content });
 
+  const result = await request;
+  console.log('Data saved to DB', result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
